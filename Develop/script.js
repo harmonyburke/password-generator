603c7e746d0efeeb01e05d19f2f08c40eeabe365
Harmony Burke <theharmonyburke@gmail.com>
2023-09-30 18:15:03 -0500

    added else if statements to all password characteristics

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 var passwordLength=(7>129);
 var fontCase= ["Uppercase","Lowercase"]
-var numbers=true 
+var numbers=true
 var specialChar=["!","@","#","$","%","^","&","*"]
 // added variables for the elements of the password
 // var random=Math.floor(Math.random()=(x>7)(x<129));
@@ -10,19 +10,40 @@ var specialChar=["!","@","#","$","%","^","&","*"]
 // Write password to the #password input
 function writePassword() {
    prompt("How long would you like your password to be? Please choose a length between 8-128 characters.");{
-    if (passwordLength=true){
-      prompt("Got it! Thanks!")
-    } else if (passwordLength=false){
-      prompt("Please enter a number between 8-128!")
+    if (passwordLength===true){
+      prompt("Got it! Thanks!");
+    } else if (passwordLength===false){
+      prompt("Please enter a number between 8-128!");
     }
     // if statements to tell the comp what to do if the answer is accepted or not 
    }
   //  added if statement to prompt to show if answer is within given parameters
    
   
-  prompt("Would you like your password to be lowecase, uppercase, or both?");
-  prompt("Would you like to include any numbers?");
-  prompt("Would you like to include any special characters(ex.!@#$%)?");
+  prompt("Would you like your password to be lowercase, uppercase, or both?");{
+    if (fontCase===("Uppercase"&&"Lowercase"&&"Both")){
+      prompt("Thanks!");
+    } else if (fontCase===false){
+      prompt("Please enter a valid answer.");
+    }
+  }
+  // added else if statements for prompts to function correctly 
+  prompt("Would you like to include any numbers?");{
+    if (numbers===true){
+      prompt ("Thanks");
+    } else if (numbers===false){
+      prompt ("Enter a valid response!");
+    }
+  }
+  // added else if statements for prompts to function correctly
+  prompt("Would you like to include any special characters(ex.!@#$%)?");{
+    if (specialChar=(x<8)){
+      prompt("Thanks!");
+    } else if (specialChar=(x>8)){
+      prompt("Enter a valid response!");
+    }
+  }
+  //added else if statements to all password characteristics 
   // added prompts for information on password
   var password = generatePassword()
   var passwordText = document.querySelector("#password");
