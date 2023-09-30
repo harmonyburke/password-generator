51594b25f1d593214640d63801080633c6ed4675
Harmony Burke <theharmonyburke@gmail.com>
2023-09-30 11:46:00 -0500

    declared function on generatePassword

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,20 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() 
-  var password = generatePassword()
-  
+function writePassword() {
+  var password = generatePassword(){ 
+    alert("password must be between 8-128 characters in length") };
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-
+}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword(){ alert("password must be between 8-128 characters in length"); });
+generateBtn.addEventListener("click", writePassword);
 
+generatePassword(){
+  alert("password must be between 8-128 characters in length")
 
-
-  
-  
-
-  // generateBtn=window.addEventListener("password must be between 8-128 characters in length");
-
+}
