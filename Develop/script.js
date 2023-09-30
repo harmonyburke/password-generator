f16c4c21387663875af0956452da2024d0c65b6f
Harmony Burke <theharmonyburke@gmail.com>
2023-09-30 17:59:26 -0500

    added else if statements

--- Develop/script.js
+++ Develop/script.js
@@ -5,14 +5,24 @@ var fontCase= ["Uppercase","Lowercase"]
 var numbers=true 
 var specialChar=["!","@","#","$","%","^","&","*"]
 // added variables for the elements of the password
+// var random=Math.floor(Math.random()=(x>7)(x<129));
+// added random to randomize the number the computer pics
 // Write password to the #password input
 function writePassword() {
-   if (passwordLength.prompt("How long would you like your password to be? Please choose a length between 8-128 characters.")){ prompt("Thanks")
-
+   prompt("How long would you like your password to be? Please choose a length between 8-128 characters.");{
+    if (passwordLength=true){
+      prompt("Got it! Thanks!")
+    } else if (passwordLength=false){
+      prompt("Please enter a number between 8-128!")
+    }
+    // if statements to tell the comp what to do if the answer is accepted or not 
    }
+  //  added if statement to prompt to show if answer is within given parameters
+   
+  
   prompt("Would you like your password to be lowecase, uppercase, or both?");
   prompt("Would you like to include any numbers?");
-  prompt("Would you like to include any special characters(ex.!@#$%)?")
+  prompt("Would you like to include any special characters(ex.!@#$%)?");
   // added prompts for information on password
   var password = generatePassword()
   var passwordText = document.querySelector("#password");
