2ef3d489aa966c9556b14d8bfa982417b35159bf
Harmony Burke <theharmonyburke@gmail.com>
2023-10-01 12:43:06 -0500

    added user input var to each if else statement to give the computer something to compare itself to

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var passwordLength=(7>129);
-var fontCase= ["Uppercase","Lowercase"];
+var fontCase= ["uppercase","lowercase","both"];
 var numbers=true;
 var specialChar=["!","@","#","$","%","^","&","*"];
 // added variables for the elements of the password
 // added random to randomize the number the computer pics
 // Write password to the #password input
 function writePassword() {
+
    prompt("How long would you like your password to be? Please choose a length between 8-128 characters.");{
-    if (passwordLength===true){
+   
+    var lengthChoice=(7>129);
+     // added var userChoice to give the comp something to compare the user answer to
+    
+     if (lengthChoice===passwordLength){
       prompt("Got it! Thanks!");
-    } else if (passwordLength===false){
+    
+    } else if (lengthChoice!=passwordLength){
       prompt("Please enter a number between 8-128!");
     }
     // if statements to tell the comp what to do if the answer is accepted or not 
@@ -20,12 +26,14 @@ function writePassword() {
    
   
   prompt("Would you like your password to be lowercase, uppercase, or both?");{
-    if (fontCase===("Uppercase"&&"Lowercase"&&"Both")){
+    var fontChoice=["uppercase", "lowercase", "both"]
+    if (fontChoice==+fontCase){
       prompt("Thanks!");
-    } else if (fontCase===false){
+    } else if (fontChoice!==fontCase){
       prompt("Please enter a valid answer.");
     }
   }
+  //added fontChoice var for user answer comparison
   // added else if statements for prompts to function correctly 
   prompt("Would you like to include any numbers?");{
     if (numbers===true){
@@ -53,5 +61,5 @@ function writePassword() {
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword());
+generateBtn.addEventListener("click", writePassword);
 
